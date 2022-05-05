e067ecc086560f275fff838788037e043586d2b0
ANA BEATRIZ ADRIANO DA SILVA <ana.silva68@alunos.sc.senac.br>
2022-05-05 14:10:38 -0300

    Criação de um teste

--- POO/1_oo.php
+++ POO/1_oo.php
@@ -1,21 +1,31 @@
 <?php
     // Criação de uma classe
-    class Humano /* Criação de Uma Classe - Todo nome de Classe começa com letra maiuscula */ */ {
-        public $cor_dos_olhos = ''; // public diz que essa informação pode ser usada dentro de uma classe
-        private $altura = ''; // private diz que essa informaçõa só pode ser usada dentro da classe
+    class Humano /* Criação de Uma Classe - Todo nome de Classe começa com letra maiúscula */ {
+        private $cor_dos_olhos; // public diz que essa informação pode ser usada dentro e fora de uma classe
+        private $altura; // private diz que essa informação só pode ser usada dentro da classe
+
+            // método construtor
+        public function __construct() {
+            $this -> altura = 1.75;
+            $this -> cor_dos_olhos = 'Rosa';
+        }
+
+        public function exibeCorDosOlhosEAltura() {
+            echo 'Cor dos olhos: ' . $this -> cor_dos_olhos;
+            echo '<br>Altura: ' . $this -> altura;
+        }
+        
         public function andar() { // o function serve para criar uma função
+            $this -> altura = 1.75;
             echo 'O humano está correndo!';
         }
     }
-
-    // Criação de um Objeto
-    $pessoa01 = new Humano(); 
-    $pessoa01 -> cor_dos_olhos = 'marron'; 
-    $pessoa01 -> andar(); 
-    echo "<br>";
-    echo $pessoa01 -> cor_dos_olhos;
-
-    $pessoa01 -> cor_dos_olhos - 'Roxo';
+    
+    // Criação de um Objeto / Seção de Teste
+    $pessoa01 = new Humano();
+    $pessoa01 -> andar();
+    echo '<br>';
+    $pessoa01 -> exibeCorDosOlhosEAltura();
     
     // Sempre que um objeto quiser acessar uma classe é utilizado uma flechinha
 ?>
